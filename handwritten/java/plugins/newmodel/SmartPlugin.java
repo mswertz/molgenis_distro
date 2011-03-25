65ce0ca13e45b5c1bc3dbe27d0572d3193080423
Morris Swertz <m.a.swertz@rug.nl>
2011-03-25 03:33:11 +0000

    added example.
    git-svn-id: http://www.molgenis.org/svn/molgenis_distro/trunk@7114 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/java/plugins/newmodel/SmartPlugin.java
@@ -0,0 +1,36 @@
+/* Date:        December 24, 2010
+ * Template:	PluginScreenJavaTemplateGen.java.ftl
+ * generator:   org.molgenis.generators.ui.PluginScreenJavaTemplateGen 3.3.3
+ * 
+ * THIS FILE IS A TEMPLATE. PLEASE EDIT :-)
+ */
+
+package plugins.newmodel;
+
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.ui.ScreenModel;
+import org.molgenis.framework.ui.html.ActionInput;
+import org.molgenis.framework.ui.html.StringInput;
+import org.molgenis.util.Tuple;
+
+public class SmartPlugin extends org.molgenis.framework.ui.GenericPlugin
+{
+	public StringInput nameInput = new StringInput("name");
+	public ActionInput helloInput = new ActionInput("setName");
+
+	public SmartPlugin(String name, ScreenModel parent)
+	{
+		super(name, parent);
+	}
+	
+	/** Action setName: set name to hello */
+	public void setName(Database db, Tuple request)
+	{
+		this.nameInput.setValue(request.getString("name"));
+	}
+	
+	public String render()
+	{
+		return super.render("SmartPlugin.ftl");
+	}
+}
